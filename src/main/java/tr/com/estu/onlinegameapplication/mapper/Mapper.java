512d7b92f1cf5a1058f6b201a5225e704f76e276
Muzaffer Arda Uslu <53192718+ardasdasdas@users.noreply.github.com>
2024-05-11 01:08:14 +0300

    create generic mapper class

---
+++ src/main/java/tr/com/estu/onlinegameapplication/mapper/Mapper.java
@@ -0,0 +1,28 @@
+package tr.com.estu.onlinegameapplication.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+
+import java.util.List;
+
+public class Mapper {
+    private static final ModelMapper modelMapper = new ModelMapper();
+
+    static {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STANDARD);
+    }
+
+    public static <S, D> D map(S source, Class<D> destinationType) {
+        return modelMapper.map(source, destinationType);
+    }
+
+    public static <S, D> List<D> mapList(List<S> sourceList, Class<D> destinationType) {
+        return sourceList.stream()
+                .map(source -> modelMapper.map(source, destinationType))
+                .toList();
+    }
+
+    private Mapper() {
+        throw new IllegalStateException("Mapper class should not be instantiated!");
+    }
+}
